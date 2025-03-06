0b8d47e50ce91021e0a50c50d2245a84186ad58e
Omarabdullah99 <omarabdullah917303@gmail.com>
2025-03-06 08:21:35 +0600

    data folder e json file add and Hero section done

--- app/page.js
+++ app/page.js
@@ -1,8 +1,10 @@
+import Hero from "@/components/Hero";
 
 
 export default function Home() {
   return (
-  <div> 
+  <div>
+    <Hero /> 
     <h1 className="text-center text-6xl">hello</h1> 
     </div>
   );
